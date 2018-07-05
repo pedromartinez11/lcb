2311d1e436d4f7c28e0925f5a4281dee10bd0a7e
Pedro M <pedromartinez11@gmail.com>
2018-07-04 17:26:13 -0700

    fix store when dev tools extension is not installed

--- src/store/initStore.js
+++ src/store/initStore.js
@@ -1,4 +1,5 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers/rootReducer';
 
 const initialState = {
@@ -9,11 +10,7 @@ const initialState = {
 
 const initStore = function () {
   const middleware = [];
-  const enhancers = compose (
-    applyMiddleware (...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ ()
-  );
+  const enhancers = composeWithDevTools (applyMiddleware (...middleware));
   let store = createStore (rootReducer, initialState, enhancers);
   return store;
 };
